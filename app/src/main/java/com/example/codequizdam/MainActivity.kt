68ee5970e71e2300146b08835cf2764ac07519d9
munah <sitysimun@gmail.com>
2025-12-30 09:26:04 +0700

    initial app

---
+++ app/src/main/java/com/example/codequizdam/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.codequizdam
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.codequizdam.ui.theme.CodeQuizDAMTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint ("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+       setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+
+        btnStart.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CodeQuizDAMTheme {
+        Greeting("Android")
+    }
+}
